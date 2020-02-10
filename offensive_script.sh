cd1a062f9ab370e914cc8b036e13a50ecbfae79f
nullenc0de <pseekamp@gmail.com>
2020-02-09 22:30:45 -0500

    Update offensive_script.sh

--- offensive_script.sh
+++ offensive_script.sh
@@ -68,6 +68,10 @@ export GOPATH=/opt/unfurl
 go get -u github.com/tomnomnom/hacks/unfurl
 ln -s /opt/unfurl/bin/unfurl /usr/local/bin/unfurl
 
+export GOPATH=/opt/anti-burl
+go get -u github.com/tomnomnom/hacks/anti-burl
+ln -s /opt/anti-burl/bin/anti-burl /usr/local/bin/anti-burl
+
 export GOPATH=/opt/ffuf
 go get -u github.com/ffuf/ffuf
 ln -s /opt/ffuf/bin/ffuf /usr/local/bin/ffuf
