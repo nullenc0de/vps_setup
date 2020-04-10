81439ece21e3ac4aaf5530bbea7a99b39f87f961
nullenc0de <pseekamp@gmail.com>
2020-04-09 21:11:08 -0400

    Update offensive_script.sh

--- offensive_script.sh
+++ offensive_script.sh
@@ -81,8 +81,12 @@ go get -u github.com/tomnomnom/hacks/unfurl
 ln -s /opt/unfurl/bin/unfurl /usr/local/bin/unfurl
 
 export GOPATH=/opt/anti-burl
+go get -u github.com/codeexpress/cidr2ip
+ln -s /opt/cidr2ip/bin/cidr2ip /usr/local/bin/anti-burl
+
+export GOPATH=/opt/cidr2ip
 go get -u github.com/tomnomnom/hacks/anti-burl
-ln -s /opt/anti-burl/bin/anti-burl /usr/local/bin/anti-burl
+ln -s /opt/anti-burl/bin/anti-burl /usr/local/bin/cidr2ip
 
 export GOPATH=/opt/ffuf
 go get -u github.com/ffuf/ffuf
